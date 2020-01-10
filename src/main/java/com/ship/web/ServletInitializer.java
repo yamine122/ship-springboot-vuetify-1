55c9f7134494c9ba30be61da85c354018d401bd5
yamine122 <wjdtmd92@gmail.com>
2020-01-10 16:39:27 +0900

    d
    d

--- src/main/java/com/ship/web/ServletInitializer.java
+++ src/main/java/com/ship/web/ServletInitializer.java
@@ -2,7 +2,7 @@ package com.ship.web;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+//22
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
