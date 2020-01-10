7643b2f23dd379b6c1fa7f37408af73138fa86fc
yamine122 <wjdtmd92@gmail.com>
2020-01-10 16:19:33 +0900

    lee
    lee

--- src/main/java/com/ship/web/ShipSpringbootVuetify1Application.java
+++ src/main/java/com/ship/web/ShipSpringbootVuetify1Application.java
@@ -2,7 +2,7 @@ package com.ship.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//lee
 @SpringBootApplication
 public class ShipSpringbootVuetify1Application {
 
