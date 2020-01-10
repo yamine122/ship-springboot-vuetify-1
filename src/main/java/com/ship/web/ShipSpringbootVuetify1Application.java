af5c005e755de06048cfbbb6ee7992edb71fa206
jdy0642 <jdy0642@gmail.com>
2020-01-10 15:50:56 +0900

    Initial commit

---
+++ src/main/java/com/ship/web/ShipSpringbootVuetify1Application.java
@@ -0,0 +1,13 @@
+package com.ship.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipSpringbootVuetify1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipSpringbootVuetify1Application.class, args);
+	}
+
+}
