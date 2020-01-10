a76260c4e11bac2c9941b3be7ac6d6969e5e602c
sungchul hong <53929474+hongsc1991@users.noreply.github.com>
2020-01-10 16:03:56 +0900

    HONG
    HONG

--- src/main/java/com/ship/web/ServletInitializer.java
+++ src/main/java/com/ship/web/ServletInitializer.java
@@ -8,6 +8,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(ShipSpringbootVuetify1Application.class);
+		//HONG TEST
 	}
 
 }
